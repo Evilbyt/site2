24944cf3e4a37d931b05d97d19b062e68e370ac7
Evilbyt <www.sumithjohn1947@gmail.com>
2025-07-16 01:38:36 +0530

    Delete js directory

--- js/particles.js
+++
@@ -1,8 +0,0 @@
-// particles.js configuration
-// You can download the full particles.js library from https://github.com/VincentGarreau/particles.js
-// This is just a sample configuration that matches what we're using in scripts.js
-
-// The actual particles.js library is quite large, so it's better to include it via CDN in your HTML:
-// <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-
-// The configuration in scripts.js handles the initialization
